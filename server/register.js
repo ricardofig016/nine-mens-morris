9e19054f27b19af2339b3579e7ff7cceabae24c3
Ricardo Figueiredo <ricardocastrofigueiredo@gmail.com>
2024-12-14 17:06:45 +0000

    create necessary files in data/ for server modules register and ranking

--- server/register.js
+++ server/register.js
@@ -1,23 +1,27 @@
-const fs = require('fs');
-const crypto = require('crypto');
+const fs = require("fs");
+const crypto = require("crypto");
 
 const register = (req, res, { nick, password }) => {
-    if (!nick || !password) {
-        res.statusCode = 400;
-        return res.end(JSON.stringify({ error: 'Missing nick or password' }));
-    }
+  if (!nick || !password) {
+    res.statusCode = 400;
+    return res.end(JSON.stringify({ error: "Missing nick or password" }));
+  }
 
-    const users = JSON.parse(fs.readFileSync('./data/users.json', 'utf-8') || '{}');
-    if (users[nick]) {
-        if (users[nick] !== hashPassword(password)) {
-            res.statusCode = 401;
-            return res.end(JSON.stringify({ error: 'Incorrect password' }));
-        }
-    } else {
-        users[nick] = hashPassword(password);
-        fs.writeFileSync('./data/users.json', JSON.stringify(users));
+  const users = JSON.parse(fs.readFileSync("./data/users.json", "utf-8") || "{}");
+  if (users[nick]) {
+    if (users[nick] !== hashPassword(password)) {
+      res.statusCode = 401;
+      return res.end(JSON.stringify({ error: "Incorrect password" }));
     }
+  } else {
+    users[nick] = hashPassword(password);
+    fs.writeFileSync("./data/users.json", JSON.stringify(users));
+  }
 
-    res.statusCode = 200;
-    res.end(JSON.stringify({ success: true }));
+  res.statusCode = 200;
+  res.end(JSON.stringify({ success: true }));
 };
+
+const hashPassword = (password) => crypto.createHash("sha256").update(password).digest("hex");
+
+module.exports = register;
